df87b3ddcffffb70f2c4b6f940b1046c33af1a49
DiurnalDays <amjiao@umich.edu>
2023-11-06 15:19:48 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -1,11 +1,19 @@
 var video;
 
+let vid = document.getElementById("video");
+
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
-
+	vid.autoplay = true;
+	vid.load();
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
-
+document.querySelector("#play").addEventListener("click", function() {
+console.log("Play Video");
+vid.play();
+});
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	vid.pause();
+	});
+	
