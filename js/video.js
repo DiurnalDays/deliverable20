34ddf4dea35041ca576848fcd0d0bd601f276a3a
DiurnalDays <amjiao@umich.edu>
2023-11-08 09:34:54 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -3,15 +3,16 @@ var video = document.querySelector("#player1")
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 
-	//video.autoplay = false;
+	video.autoplay = false;
 	video.load();
 	video.loop=false
+	video.volume=document.querySelector("#slider").value/100;
+	});
 	document.querySelector("#play").addEventListener("click", function() {
 		console.log("Play Video");
 		video.play();
-		videoElement.onvolumechange = () => {
-			const newVolume = video.volume;
-	}});
+		document.querySelector("#volume").textContent = Math.round(video.volume*100) + "%";
+	});
 	document.querySelector("#pause").addEventListener("click", function() {
 		console.log("Pause Video");
 		video.pause();
@@ -20,6 +21,10 @@ window.addEventListener("load", function() {
 			video.playbackRate*=.9;
 			console.log("The current playback speed is " + video.playbackRate);
 		});
+	document.querySelector("#faster").addEventListener("click", function(){
+		video.playbackRate/=.9;
+		console.log("New speed is " + video.playbackRate);
+	});
 	document.querySelector("#skip").addEventListener("click", function(){
 			video.currentTime+=10;
 			console.log("The current time of the video is " + video.currentTime)
